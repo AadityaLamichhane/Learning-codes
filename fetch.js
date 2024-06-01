0235edd0705a593c1c5a4b2d97307304b4188e5d
AadityaLamichhane <146906370+AadityaLamichhane@users.noreply.github.com>
2024-06-01 14:52:03 +0545

    Fixing  fetch.js
    Todo list was updated in this .

--- fetch.js
+++ fetch.js
@@ -1,6 +1,5 @@
 /*API to get or feth the data from the difrrent website
-how to hit the web page with the html and js 
- */
+*/
 async function authenticator()
 {
     const responce = await fetch ("https://fakerapi.it/api/v1/persons");//this await for the rsponce of the fetching the data 
