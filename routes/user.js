cfd8e7eb1d6fd8a120d8e4c1d31ea7a15792e3b3
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2024-06-10 17:02:31 +0545

    Fixing my mistakes

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,150 @@
+const { Router, response } = require("express");
+const router = Router();
+const userMiddleware = require("../middleware/user");
+const {User ,key,Course} = require("../db"); 
+const jwtwebtoken = require("jsonwebtoken");
+// User Routes
+router.post('/signup', async (req, res) => {
+    const {username , password } = req.body;
 
-const {User} =require("../db/index.js");
-const {key} = require("../db/index.js");
-const jwtwebtoken = require('jsonwebtoken');
-
-async function userMiddleware(req, res, next) {
-    const {authorization} = req.headers;
-    if(!authorization)
+    if(!username || !password)
         {
-            return res.status(400).json({massage:"No Credential was given to the server"});
+            return res.status(410).json({massage:"NO username or password "});
         }
-        //Checking the verification of the  user
-        try{
-            const verify_user =   jwtwebtoken.verify(authorization,key);
-            console.log(`This is verified user that are verified using the jwt key ${verify_user}`);
+            try{
+
+                const userExist = await User.findOne({username:username});
+                  
+                if(userExist)
+                    {
+                        return res.status(400).json({massage:"User with the credential already exist"});
+                    }                 
+                    const  signed =  jwtwebtoken.sign({username:username},key);
+
+                const newuser = new User({
+                    username:username,
+                    password:password
+                }); 
+                await newuser.save();
+                return res.status(200).json({massage:"Successfult signed up ",token:signed});
+
+            }
+            catch(error)
+            {
+                console.error(error);
+                return res.status(500).json({ message: "Something went wrong while saving the data" });
+            }
             
-            const user_exist =  await User.findOne({username:verify_user.username});
-            if(!user_exist)
+            
+    // Implement user signup logic
+});
+
+router.post('/signin',async (req, res) => {
+    const { username , password } = req.body;
+    if(!username || !password)
+        {
+            return res.status(410).json({massage:"NO username or password "});
+        }
+
+    const userExist = await User.findOne({username:username});
+
+    
+
+    if(!userExist)
+            {
+                return res.status(400).json({massage:"User with the credential Dont exist"});
+            }
+
+            if(userExist.password !== password)
                 {
-                    return res.status(400).json({massage:"User donnt exst "});
+                    return res.status(400).json({masssage:"Incorrect password"})
                 }
-        next();
-            
+
+    try {
+            const token = jwtwebtoken.sign({username:username},key);
+            return res.status(200).json({token:token});
         }
         catch{
-            return res.status(400).json({massage:"Something went wrong Couldnot Veriify You"});
+            return res.status(400).json({masasage:"signing fail with jwt"});
+        }
+        
+    // Implement admin signup logic
+});
+
+router.get('/courses', async (req, res) => {
+    const courses_to_show =await Course.find();
+    
+    if(!courses_to_show)
+        {
+            return res.status(404).json({massgae:"no courses to show"});
+
         }
 
-        //Checking if the  user exist previously or not 
-      
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
-}
+        courses_to_show.map((element)=>{
+            return element.title;
+        });
+        res.status(200).json({All_courses : courses_to_show});
 
-module.exports = userMiddleware;
+});
+
+router.post('/courses/:courseId', userMiddleware,async (req, res) => {
+    const id = req.params.courseId;
+    const {Authorization} = req.headers;
+    let user = '';
+
+
+    //Geting the data of the user 
+    
+    try{
+        const  verify_user = jwtwebtoken.verify(Authorization,key);
+        user = verify_user.username;
+
+    }
+    catch{
+        return res.status(400).json({massage:"Somwhing went wrong while checking credential"})
+    }
+
+    //Checking the user course history with the same course
+    
+    const courseofuser = user.myCourse;
+    
+    const user_have_course = courseofuser.filter((element)=>
+    {
+        if(element==id)
+            {
+                return true;
+            }
+    });
+
+    if(Object.values(user_have_course).length!==0)
+        {
+            return res.status(300).json({massage:"already brough that course"}); 
+        }
+    
+
+    
+    //if my user have that 
+
+    const CourseWithId =await  Course.findOne({_id:id});
+    if(!CourseWithId)
+        {
+            return res.status(400).json({massage:"No course with that id was found"});
+        }
+        try{
+            user.myCourse.push(id);
+            await user.save();
+
+        }
+        catch(error){
+            return res.json({massage:"something went wrong in the course purchasing",error:error});
+        }
+        
+       
+    // Implement course purchase logic
+});
+
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
+    // Implement fetching purchased courses logic
+});
+
+module.exports = router
