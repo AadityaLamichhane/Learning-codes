968daa79a9641f3c22b6af28ce50e48d4bbd0111
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2024-06-10 17:08:49 +0545

    Updated code Sorry for inconvenience

--- 04jwtmongo/middleware_jwt/admin.js
+++ 04jwtmongo/middleware_jwt/admin.js
@@ -5,10 +5,11 @@ const jwtwebtoken = require('jsonwebtoken');
 // Middleware for handling auth
 async function adminMiddleware(req, res, next) {
 
-    const authorize = req.headers.Authorization;
+    const {authorization} = req.headers;
 
     ///checking the field
-    if(!authorize)
+    
+    if(!authorization)
         {
             return res.status(404).json({massage:"Empty credential to account"});
         }
@@ -16,21 +17,21 @@ async function adminMiddleware(req, res, next) {
         //geting the verification for the user 
 
     try {
+        const verifiied_profile =  jwtwebtoken.verify(authorization,key);
         
-
-        const verifiied_profile =  jwtwebtoken.verify(authorize,key);
+        const user_data =await  Admin.findOne({username:verifiied_profile.username});
+        
+        if(!user_data)
+            {
+                return res.status(400).json({massage:"NO user was found with that credetial "});
+            }
     }
     catch(error)
     {
         return res.status(500).json({massage:"Something went wrong on verification of the profile"});
     }
-    //Checking if the given user exist or not 
-
-    const user_data =await  User.findOne({username:authorize.username});
-    if(!user_data)
-        {
-            return res.status(400).json({massage:"NO user was found with that credetial "});
-        }
+    
+   
         //All sorted
         next();
 
--- 04jwtmongo/route_jwt/user.js
+++ 04jwtmongo/route_jwt/user.js
@@ -1,26 +1,35 @@
-const { Router } = require("express");
-const router = Router();
-const userMiddleware = require("../middleware/user");
 
-// User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
-});
+const {User} =require("../db/index.js");
+const {key} = require("../db/index.js");
+const jwtwebtoken = require('jsonwebtoken');
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
-});
+async function userMiddleware(req, res, next) {
+    const {authorization} = req.headers;
+    if(!authorization)
+        {
+            return res.status(400).json({massage:"No Credential was given to the server"});
+        }
+        //Checking the verification of the  user
+        try{
+            const verify_user =   jwtwebtoken.verify(authorization,key);
+            console.log(`This is verified user that are verified using the jwt key ${verify_user}`);
+            
+            const user_exist =  await User.findOne({username:verify_user.username});
+            if(!user_exist)
+                {
+                    return res.status(400).json({massage:"User donnt exst "});
+                }
+        next();
+            
+        }
+        catch{
+            return res.status(400).json({massage:"Something went wrong Couldnot Veriify You"});
+        }
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
-});
+        //Checking if the  user exist previously or not 
+      
+    // Implement user auth logic
+    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+}
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
-});
-
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
-});
-
-module.exports = router
+module.exports = userMiddleware;
