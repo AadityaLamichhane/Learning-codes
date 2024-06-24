f64b520f196e27ae3dd6755134a966b9e5c31009
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2024-06-24 22:36:50 +0545

    Add cpp file

---
+++ Teacher.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class Teacher{
+string name ;
+string dept ;
+string subject;
+double salary;
+
+void ChangeDept(string newDept)
+{
+    dept = newDept
+}
+};
+int main()
+{
+    Teacher t1;
+    
+}
